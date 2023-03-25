cf53d793df7bcf77d24cced883a4b76f42a043b1
João Paulo <jpcchaves@outlook.com>
2023-03-25 18:30:19 -0300

    feat: add toast factory

---
+++ src/modules/auth/_core/factories/toast/makeToastFactory.tsx
@@ -0,0 +1,33 @@
+import {ToastPositionWithLogical, useToast} from "@chakra-ui/react";
+
+export enum ToastStatus {
+    info = "info",
+    warning = "warning",
+    success = "success",
+    error = "error"
+}
+
+const Toast = () => {
+
+    const toast = useToast()
+    const makeToast = (title: string,
+                       description: string,
+                       status: ToastStatus,
+                       duration: number,
+                       position: ToastPositionWithLogical,
+                       isClosable: boolean,
+    ) => {
+        toast({
+            title,
+            description,
+            status,
+            duration,
+            position,
+            isClosable
+        })
+    }
+
+    return [makeToast] as const;
+};
+
+export default Toast;
