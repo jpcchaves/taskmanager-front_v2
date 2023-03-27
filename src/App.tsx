6e61289330c3003106b6527c88244a2ea5ded583
João Paulo <jpcchaves@outlook.com>
2023-03-26 21:15:13 -0300

    feat: updated react to v18

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Route, Routes} from "react-router-dom";
+import SignIn from "./modules/auth/pages/signIn";
+import SignUp from "./modules/auth/pages/signUp";
+import PrivateRoutes from "./store/app/components/RequireAuth";
+import AdminLayout from "./layouts/admin";
+import MainDashboard from "./modules/admin/default";
+import Marketplace from "./modules/admin/marketplace";
+import {store} from "./store/app/store";
+import theme from "./theme/theme";
+import {Provider} from "react-redux";
+import {ChakraProvider} from "@chakra-ui/react";
+
+const App = () => {
+    return (
+        <Provider store={store}>
+            <ChakraProvider theme={theme}>
+                <Routes>
+                    {/* Public Routes */}
+                    <Route path='/login' element={<SignIn/>}/>
+                    <Route path='/register' element={<SignUp/>}/>
+
+                    {/* Protected Routes */}
+                    <Route element={<PrivateRoutes/>}>
+                        <Route element={<AdminLayout/>} path='/*'>
+                            <Route element={<MainDashboard/>} path='tasks'/>
+                            <Route element={<Marketplace/>} path='dashboard'/>
+                        </Route>
+                    </Route>
+                </Routes>
+            </ChakraProvider>
+        </Provider>
+    );
+};
+
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,7 @@
 import {defineConfig} from 'vite'
-import reactRefresh from '@vitejs/plugin-react'
-import svgrPlugin from 'vite-plugin-svgr'
+import react from '@vitejs/plugin-react-swc'
 
-import path from 'path'
-
+// https://vitejs.dev/config/
 export default defineConfig({
-    build: {
-        outDir: 'build',
-    },
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src')
-        }
-    },
-    plugins: [
-        reactRefresh(),
-        svgrPlugin({
-            svgrOptions: {
-                icon: true,
-            },
-        }),
-    ],
+    plugins: [react()],
 })
