ff6a1d003eb13b35a0725c00081110c3f86509a1
João Paulo <jpcchaves@outlook.com>
2023-04-03 11:24:42 -0300

    refactor: adjust main tsx to react 17

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import {createRoot} from "react-dom/client";
+import ReactDOM from 'react-dom';
 
 import {BrowserRouter} from 'react-router-dom';
 
 import './assets/css/App.css';
 import App from "./App";
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
             <App/>
         </BrowserRouter>
-    </React.StrictMode>
+    </React.StrictMode>,
+    document.getElementById('root')
 )
