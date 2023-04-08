d68d1fcc14b38308f9488c8e8307e9e051e78599
João Paulo <jpcchaves@outlook.com>
2023-04-08 19:04:57 -0300

    feat: add toast to notify an error in login

--- src/contexts/auth/provider/AuthProvider.tsx
+++ src/contexts/auth/provider/AuthProvider.tsx
@@ -1,73 +1,85 @@
-import React, {useState} from 'react';
-import {User} from "../../../types/user/User";
+import React, { useState } from "react";
+import { User } from "../../../types/user/User";
 import AuthService from "../../../services/auth/AuthService";
-import {AuthContext} from "../context/AuthContext";
-import {ILoginResponse} from "../../../modules/auth/models/login/ILoginResponse";
-import {UserLoginRequest} from "../../../types/user/login/UserLoginRequest";
-import {SessionStorageUtils} from "../../../modules/auth/utils/cache/SessionStorageUtils";
-import Toast, {ToastStatus} from "../../../modules/auth/_core/factories/toast/makeToastFactory";
+import { AuthContext } from "../context/AuthContext";
+import { ILoginResponse } from "../../../modules/auth/models/login/ILoginResponse";
+import { UserLoginRequest } from "../../../types/user/login/UserLoginRequest";
+import { SessionStorageUtils } from "../../../modules/auth/utils/cache/SessionStorageUtils";
+import Toast, {
+  ToastStatus,
+} from "../../../modules/auth/_core/factories/toast/makeToastFactory";
 
 interface IProps {
-    children: JSX.Element
+  children: JSX.Element;
 }
 
-const AuthProvider = ({children}: IProps) => {
-    const [user, setUser] = useState<User | null>(null);
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [makeToast] = Toast();
+const AuthProvider = ({ children }: IProps) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [makeToast] = Toast();
 
-    const isValidLoginResponse = (args: ILoginResponse): boolean => {
-        return !!(args.user && args.tokenType && args.accessToken);
+  const isValidLoginResponse = (args: ILoginResponse): boolean => {
+    return !!(args.user && args.tokenType && args.accessToken);
+  };
+
+  const login = async (data: UserLoginRequest) => {
+    setIsLoading(true);
+    const valuesToSubmit = {
+      usernameOrEmail: data.usernameOrEmail,
+      password: data.password,
+    };
+
+    try {
+      const { data: res } = await AuthService.login(valuesToSubmit);
+
+      setIsLoading(false);
+      makeToast(
+        "Usuário autenticado com sucesso!",
+        "Você será redirecionado para o dashboard",
+        ToastStatus.success,
+        3000,
+        "top-right",
+        true
+      );
+
+      if (data.remember) {
+        SessionStorageUtils.saveUserAndToken(res);
+      } else {
+        SessionStorageUtils.saveToken("accessToken", res.accessToken);
+      }
+
+      if (isValidLoginResponse(res)) {
+        setUser(res.user);
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e: any) {
+      makeToast(
+        "Ocorreu um erro!",
+        e?.response?.data?.message ||
+          "Ocorreu um erro inesperado! Por favor, tente novamente.",
+        ToastStatus.error,
+        3000,
+        "top-right",
+        true
+      );
+      setIsLoading(false);
+      console.log(e);
     }
+  };
 
-    const login = async (data: UserLoginRequest) => {
-        setIsLoading(true);
-        const valuesToSubmit = {
-            usernameOrEmail: data.usernameOrEmail,
-            password: data.password
-        }
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+    localStorage.removeItem("accessToken");
+  };
 
-        try {
-            const {data: res} = await AuthService.login(valuesToSubmit);
-
-            setIsLoading(false)
-            makeToast("Usuário autenticado com sucesso!",
-                "Você será redirecionado para o dashboard",
-                ToastStatus.success,
-                3000, "top-right",
-                true)
-
-            if (data.remember) {
-                SessionStorageUtils.saveUserAndToken(res);
-            } else {
-                SessionStorageUtils.saveToken("accessToken", res.accessToken);
-            }
-
-            if (isValidLoginResponse(res)) {
-                setUser(res.user)
-                return true;
-            } else {
-                return false;
-            }
-        } catch (e) {
-            setIsLoading(false);
-            console.log(e)
-        }
-
-    }
-
-    const logout = () => {
-        setUser(null);
-        localStorage.removeItem("user");
-        localStorage.removeItem("accessToken");
-    }
-
-
-    return (
-        <AuthContext.Provider value={{user, isLoading, login, logout}}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={{ user, isLoading, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
