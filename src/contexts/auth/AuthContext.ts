8e009b0d0081f59b8c4d66c9b45b4fb89553e2f2
João Paulo <jpcchaves@outlook.com>
2023-04-08 12:19:16 -0300

    feat: add auth context

---
+++ src/contexts/auth/AuthContext.ts
@@ -0,0 +1,11 @@
+import {createContext} from 'react';
+import {User} from "../../types/user/User";
+import {UserLoginRequest} from "../../types/user/login/UserLoginRequest";
+
+export type AuthContextType = {
+    user: User | null;
+    login: (args: UserLoginRequest) => Promise<boolean>;
+    logout: () => void;
+};
+
+export const AuthContext = createContext<AuthContextType>(null);
