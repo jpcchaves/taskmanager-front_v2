4ebda1d69ada6aaa80fd171d3a29f40fe76d47a7
João Paulo <jpcchaves@outlook.com>
2023-04-09 11:31:20 -0300

    feat: add crud operations to task service, todo: impl them

--- src/modules/admin/tasks/_core/services/impl/TasksServiceImpl.ts
+++ src/modules/admin/tasks/_core/services/impl/TasksServiceImpl.ts
@@ -2,10 +2,28 @@ import { TasksService } from "../models/TasksService";
 import { AxiosResponse } from "axios";
 import http from "../../../../../../http-common/http";
 import { TasksPaginated } from "../../../../../../types/tasks/TasksPaginated";
+import { Task } from "../../../../../../types/tasks/Task";
+import { TaskCreate } from "../../../../../../types/tasks/TaskCreate";
 
 class TasksServiceImpl implements TasksService {
   async getAll(): Promise<AxiosResponse<TasksPaginated>> {
     return http.get(import.meta.env.VITE_API_TASKS_ENDPOINT + "?size=100");
+  }
+
+  async create(data: TaskCreate): Promise<AxiosResponse<Task>> {
+    return Promise.resolve(undefined);
+  }
+
+  async delete(id: string): Promise<void> {
+    return Promise.resolve(undefined);
+  }
+
+  async getById(id: string): Promise<AxiosResponse<Task>> {
+    return Promise.resolve(undefined);
+  }
+
+  async update(id: string, data: Task): Promise<AxiosResponse<Task>> {
+    return Promise.resolve(undefined);
   }
 }
 
--- src/modules/admin/tasks/_core/services/models/TasksService.ts
+++ src/modules/admin/tasks/_core/services/models/TasksService.ts
@@ -1,6 +1,12 @@
 import { AxiosResponse } from "axios";
 import { TasksPaginated } from "../../../../../../types/tasks/TasksPaginated";
+import { Task } from "../../../../../../types/tasks/Task";
+import { TaskCreate } from "../../../../../../types/tasks/TaskCreate";
 
 export interface TasksService {
   getAll: () => Promise<AxiosResponse<TasksPaginated>>;
+  getById: (id: string) => Promise<AxiosResponse<Task>>;
+  create: (data: TaskCreate) => Promise<AxiosResponse<Task>>;
+  update: (id: string, data: Task) => Promise<AxiosResponse<Task>>;
+  delete: (id: string) => Promise<void>;
 }
