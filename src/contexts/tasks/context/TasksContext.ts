9d5215206ba9d3718c04c887bff1477fc478a3f0
João Paulo <jpcchaves@outlook.com>
2023-04-09 12:06:00 -0300

    feat: add create task context and provider

--- src/contexts/tasks/context/TasksContext.ts
+++ src/contexts/tasks/context/TasksContext.ts
@@ -1,10 +1,12 @@
 import { Task } from "../../../types/tasks/Task";
 import { createContext } from "react";
 import { TasksPaginated } from "../../../types/tasks/TasksPaginated";
+import { TaskCreate } from "../../../types/tasks/TaskCreate";
 
 export type TasksContextType = {
   isLoading: boolean;
   getAll: () => Promise<boolean>;
+  create: (data: TaskCreate) => Promise<boolean>;
   tasks: TasksPaginated;
   task: Task;
 };
--- src/contexts/tasks/provider/TasksProvider.tsx
+++ src/contexts/tasks/provider/TasksProvider.tsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import { Task } from "../../../types/tasks/Task";
 import TasksServiceImpl from "../../../modules/admin/tasks/_core/services/impl/TasksServiceImpl";
 import { TasksPaginated } from "../../../types/tasks/TasksPaginated";
+import { TaskCreate } from "../../../types/tasks/TaskCreate";
+import Toast, { ToastStatus } from "../../../factories/toast/makeToastFactory";
 
 interface IProps {
   children: JSX.Element;
@@ -13,8 +15,39 @@ const TasksProvider = ({ children }: IProps) => {
   const [tasks, setTasks] = useState<TasksPaginated>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
+  const [makeToast] = Toast();
+
   const toggleLoading = () => {
     setIsLoading((prevState) => !prevState);
+  };
+
+  const create = async (data: TaskCreate) => {
+    toggleLoading();
+    try {
+      await TasksServiceImpl.create(data);
+      toggleLoading();
+
+      makeToast(
+        "Task criada",
+        `Você criou a task: ${data?.task}`,
+        ToastStatus.success,
+        3000,
+        "top-right",
+        true
+      );
+      return true;
+    } catch (e: any) {
+      makeToast(
+        "Ocorreu um erro!",
+        e?.response?.data?.message ||
+          "Ocorreu um erro inesperado! Por favor, tente novamente",
+        ToastStatus.error,
+        3000,
+        "top-right",
+        true
+      );
+      return false;
+    }
   };
 
   const getAll = async () => {
@@ -32,7 +65,7 @@ const TasksProvider = ({ children }: IProps) => {
   };
 
   return (
-    <TasksContext.Provider value={{ isLoading, task, tasks, getAll }}>
+    <TasksContext.Provider value={{ isLoading, task, tasks, getAll, create }}>
       {children}
     </TasksContext.Provider>
   );
