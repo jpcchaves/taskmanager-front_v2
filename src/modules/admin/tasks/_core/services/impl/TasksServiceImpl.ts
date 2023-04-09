b66bac4922efe4f0a6f1d3ad10ab587903dea961
João Paulo <jpcchaves@outlook.com>
2023-04-09 11:13:38 -0300

    feat: add pagination to api call get all tasks

--- src/modules/admin/tasks/_core/services/impl/TasksServiceImpl.ts
+++ src/modules/admin/tasks/_core/services/impl/TasksServiceImpl.ts
@@ -5,7 +5,7 @@ import { TasksPaginated } from "../../../../../../types/tasks/TasksPaginated";
 
 class TasksServiceImpl implements TasksService {
   async getAll(): Promise<AxiosResponse<TasksPaginated>> {
-    return http.get(import.meta.env.VITE_API_TASKS_ENDPOINT);
+    return http.get(import.meta.env.VITE_API_TASKS_ENDPOINT + "?size=100");
   }
 }
 
--- src/modules/admin/tasks/index.tsx
+++ src/modules/admin/tasks/index.tsx
@@ -1,25 +1,28 @@
 // Chakra imports
-import { Box, Button, SimpleGrid } from "@chakra-ui/react";
+import { Box, SimpleGrid } from "@chakra-ui/react";
 // Assets
 // Custom components
 import { makeColumnsTasksList } from "./_core/factories/makeColumnsTasksList";
 import { Datatable } from "./components/datatable";
 import { TableWrapper } from "./components/tableWrapper";
-import { useContext, useEffect } from "react";
+import { useCallback, useContext, useEffect } from "react";
 import { TasksContext } from "../../../contexts/tasks/context/TasksContext";
 
 export default function TasksView() {
   const { isLoading, tasks, getAll } = useContext(TasksContext);
 
+  const getTasks = useCallback(async () => {
+    await getAll();
+  }, []);
+
   useEffect(() => {
-    getAll();
+    getTasks();
   }, []);
 
   return (
     <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
       <SimpleGrid columns={{ base: 1, md: 1, xl: 1 }} gap="20px">
         <TableWrapper>
-          <Button isLoading={isLoading}>fdsfafdsa</Button>
           <Datatable
             columns={makeColumnsTasksList()}
             data={tasks?.content || []}
