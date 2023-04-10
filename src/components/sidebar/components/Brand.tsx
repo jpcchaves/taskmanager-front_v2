0ca85ec226e96200ab812dcd891d955007352367
João Paulo <jpcchaves@outlook.com>
2023-04-10 14:05:06 -0300

    feat: add onClick to navigate to homepage

--- src/components/sidebar/components/Brand.tsx
+++ src/components/sidebar/components/Brand.tsx
@@ -4,15 +4,17 @@ import { Box, Flex, Image, useColorModeValue } from "@chakra-ui/react";
 // Custom components
 import { HSeparator } from "../../../components/separator/Separator";
 import logo from "../../../assets/img/logo/logo-light.png";
-import { HorizonLogo } from "../../icons/Icons";
+import { useNavigate } from "react-router-dom";
 
 export function SidebarBrand() {
   //   Chakra color mode
   let logoColor = useColorModeValue("navy.700", "white");
 
+  const navigate = useNavigate();
+
   return (
     <Flex alignItems="center" flexDirection="column">
-      <Box>
+      <Box cursor="pointer" onClick={() => navigate("/tarefas")}>
         <Image src={logo} w={150} mb={15} />
       </Box>
       <HSeparator mb="20px" />
