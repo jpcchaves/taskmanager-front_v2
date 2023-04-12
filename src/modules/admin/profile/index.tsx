1f5a78e2cc748ee3475eb5b2776d312638a414c6
João Paulo <jpcchaves@outlook.com>
2023-04-12 12:57:31 -0300

    feat: add formik validation

--- src/modules/admin/profile/index.tsx
+++ src/modules/admin/profile/index.tsx
@@ -4,6 +4,7 @@ import {
   Button,
   Divider,
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Heading,
   Input,
@@ -22,11 +23,24 @@ import { AuthContext } from "../../../contexts/auth/context/AuthContext";
 import Card from "../../../components/card/Card";
 import { generateAvatar } from "./utils/helpers/generateAvatar";
 import { trimUsernameOrName } from "./utils/helpers/trimUsernameOrName";
+import { useFormik } from "formik";
+import { updateUserValidation } from "./utils/validation/updateUserValidation";
 
 export default function ProfileOverview() {
   const brandStars = useColorModeValue("brand.500", "brand.400");
 
   const { user } = useContext(AuthContext);
+
+  const validation = useFormik({
+    enableReinitialize: true,
+    initialValues: {
+      name: user ? user.name : "",
+      currentPassword: "",
+      password: "",
+    },
+    validationSchema: updateUserValidation,
+    onSubmit: (values) => console.log(values),
+  });
 
   return (
     <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
@@ -55,11 +69,23 @@ export default function ProfileOverview() {
           </Heading>
           <Text>Atualize os dados do seu perfil</Text>
           <Divider mt="1" mb="6" />
-          <form>
+          <form
+            onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+              e.preventDefault();
+              validation.handleSubmit();
+              return false;
+            }}
+          >
             <SimpleGrid columns={{ sm: 1, md: 2, lg: 2 }} gap="20px">
               <FormControl>
                 <FormLabel>Nome</FormLabel>
-                <Input />
+                <Input
+                  variant="auth"
+                  name="name"
+                  value={validation.values.name}
+                  onChange={validation.handleChange}
+                  onBlur={validation.handleBlur}
+                />
               </FormControl>
               <FormControl>
                 <FormLabel>
@@ -68,7 +94,12 @@ export default function ProfileOverview() {
                     *
                   </Text>
                 </FormLabel>
-                <Input disabled />
+                <Input
+                  variant="auth"
+                  disabled
+                  cursor="not-allowed"
+                  value={user ? user.username : ""}
+                />
               </FormControl>
               <FormControl>
                 <FormLabel>
@@ -77,29 +108,76 @@ export default function ProfileOverview() {
                     *
                   </Text>
                 </FormLabel>
-                <Input disabled />
+
+                <Input
+                  variant="auth"
+                  disabled
+                  cursor="not-allowed"
+                  value={user ? user.email : ""}
+                />
               </FormControl>
-              <FormControl>
+              <FormControl
+                isInvalid={
+                  !!(
+                    validation.touched.currentPassword &&
+                    validation.errors.currentPassword
+                  )
+                }
+              >
                 <FormLabel>
                   Senha Atual
                   <Text display="inline" ml="1" color={brandStars}>
                     *
                   </Text>
                 </FormLabel>
-                <Input />
+                <Input
+                  variant="auth"
+                  name="currentPassword"
+                  value={validation.values.currentPassword}
+                  onChange={validation.handleChange}
+                  onBlur={validation.handleBlur}
+                  isInvalid={
+                    !!(
+                      validation.touched.currentPassword &&
+                      validation.errors.currentPassword
+                    )
+                  }
+                />
+                <FormErrorMessage>
+                  {validation.errors.currentPassword}
+                </FormErrorMessage>
               </FormControl>
-              <FormControl>
+              <FormControl
+                isInvalid={
+                  !!(validation.touched.password && validation.errors.password)
+                }
+              >
                 <FormLabel>
                   Senha Nova
                   <Text display="inline" ml="1" color={brandStars}>
                     *
                   </Text>
                 </FormLabel>
-                <Input />
+                <Input
+                  variant="auth"
+                  name="password"
+                  value={validation.values.password}
+                  onChange={validation.handleChange}
+                  onBlur={validation.handleBlur}
+                  isInvalid={
+                    !!(
+                      validation.touched.password && validation.errors.password
+                    )
+                  }
+                />
+
+                <FormErrorMessage>
+                  {validation.errors.password}
+                </FormErrorMessage>
               </FormControl>
             </SimpleGrid>
-            <SimpleGrid mt="5">
-              <Button colorScheme="brandScheme" color="white">
+            <SimpleGrid mt="16">
+              <Button colorScheme="brandScheme" color="white" type="submit">
                 Atualizar Perfil
               </Button>
             </SimpleGrid>
